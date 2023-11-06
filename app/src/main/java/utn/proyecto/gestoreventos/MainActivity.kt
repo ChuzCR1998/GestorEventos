612056868fb7982977843d5c0d29cb2000ba9e03
ChuzCR1998 <jdurena@est.utn.ac.cr>
2023-11-06 11:36:13 -0600

    Creación de proyecto

---
+++ app/src/main/java/utn/proyecto/gestoreventos/MainActivity.kt
@@ -0,0 +1,46 @@
+package utn.proyecto.gestoreventos
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import utn.proyecto.gestoreventos.ui.theme.GestorEventosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GestorEventosTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GestorEventosTheme {
+        Greeting("Android")
+    }
+}
