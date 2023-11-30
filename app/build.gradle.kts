92defa415703dcf3e005e19e0c2d4c58a984d4f6
Jesús Ureña <jesusurenablanco@gmail.com>
2023-11-29 23:42:25 -0600

    commit desde persistencia

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation ("io.github.vanpra.compose-material-dialogs:datetime:0.8.1-rc")
     coreLibraryDesugaring ("com.android.tools:desugar_jdk_libs:1.1.6")
 
+
     implementation ("io.github.vanpra.compose-material-dialogs:datetime:0.8.1-rc")
     coreLibraryDesugaring ("com.android.tools:desugar_jdk_libs:1.1.6")
 
