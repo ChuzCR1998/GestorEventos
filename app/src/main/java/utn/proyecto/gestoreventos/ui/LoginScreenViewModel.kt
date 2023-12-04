476007815b36deca2dcd9fc19a143812a3b9565a
IS. Pablo Mora M <ing.pablomora25@gmail.com>
2023-12-03 19:26:01 -0600

    Mejora en login

--- app/src/main/java/utn/proyecto/gestoreventos/ui/LoginScreenViewModel.kt
+++ app/src/main/java/utn/proyecto/gestoreventos/ui/LoginScreenViewModel.kt
@@ -1,6 +1,7 @@
 package utn.proyecto.gestoreventos.ui
 
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -11,8 +12,10 @@ import kotlinx.coroutines.launch
 
 
 class LoginScreenViewModel: ViewModel() {
-    private val auth: FirebaseAuth = Firebase.auth
+    //private val auth: FirebaseAuth = Firebase.auth
     private val _loading = MutableLiveData(false)
+
+    val auth = FirebaseAuth.getInstance()
     fun signInWithEmailAndPassword(
         email: String, password: String, home: () -> Unit
     ) = viewModelScope.launch {
@@ -20,10 +23,20 @@ class LoginScreenViewModel: ViewModel() {
             auth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
-                        Log.d("Gestor Eventos", "signInWithEmailAndPassword logueado")
+
                         home()
+                        Log.d("Gestor Eventos", "signInWithEmailAndPassword logueado")
                     } else {
-                        Log.d("Gestor Eventos", "signInWithEmailAndPassword: ${task.result.toString()}")
+                        if (task.exception?.message?.contains("no user") == true) {
+                            // Muestra un mensaje al usuario informando que no existe el usuario
+                            //showToast("El usuario no existe")
+                        } else {
+                            // Otra condición de error
+                            // Muestra otro mensaje de error o maneja la situación según tu lógica
+                            //showToast("Error al autenticar: ${task.exception?.message}")
+                        }
+
+                        Log.d("Gestor Eventos", "HOLA MUNDO")
                     }
                 }
         } catch (ex: Exception) {
